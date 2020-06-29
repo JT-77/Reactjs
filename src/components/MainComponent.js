1606a1e5f461fedf3e4b43a2ad022008850ca9f9
JT-77 <jatin7599@gmail.com>
2020-06-29 23:37:12 +0530

    SAP2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
+import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
@@ -7,6 +8,7 @@ import { COMMENTS } from '../shared/comments';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -32,12 +34,22 @@ class Main extends Component {
       );
     }
 
+    const DishWithId = ({match}) => {
+        return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId  === parseInt(match.params.dishId, 10))}
+          />
+        );
+    }
+
     return (
       <div>
         <Header />
           <Switch>
             <Route path="/home" component={HomePage} />
+            <Route exact path="/contactus" component={Contact} />
             <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+            <Route path="/menu/:dishId" component={DishWithId} />
             <Redirect to="/home" />
           </Switch>
         <Footer />
